ef0225e8834a7486f927f135f18d36fbe98084fc
Abdul Rahim <zero_degree_96@hotmail.com>
2022-12-16 17:40:06 +0800

    Set up routing and first test page

---
+++ Pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const login = () => {
+  return (
+    <View>
+      <Text>Hello this is the login page</Text>
+    </View>
+  );
+};
+
+export default login;
